4df235bfbbb514d11f9718a3937b60ff00622efd
dineshb72 <bukyadinesh72@gmail.com>
2025-09-21 20:05:27 +0000

    done

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import myImg from "../../Assets/db.png";
 import Tilt from "react-parallax-tilt";
 import {
   //AiFillGithub,
@@ -30,17 +29,18 @@ function Home2() {
   <br /><br />
   I believe great software is not just about code — it’s about creating systems that scale, adapt, and leave a lasting impact.
 </p>
-          </Col>
-          <Col md={4} className="myAvtar">
+       
+         </Col>
+         {/* <Col md={4} className="myAvtar">
             <Tilt>
               <img
                 data-aos="fade-left"
                 src={myImg}
                 className="img-fluid"
-                alt="db"
+                alt=""
               />
             </Tilt>
-          </Col>
+          </Col>  */}
         </Row>
         <Row>
           <Col md={12} className="home-about-social">
